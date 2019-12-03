a247455dd01a2a8cc58e8e6242281e61a3946220
qudcc <38711676+qudcc@users.noreply.github.com>
2019-12-03 15:04:46 +0800

    Update main.c
    add test bug

--- test/main.c
+++ test/main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+char name[256];
+void
+bug(char *p) {
+	strcpy(name, p);
+}
+	
+void 
 int
 main(int argc, char *argv[]) {
+	bug(argv[1]);
 	printf("Hello, %s!", argv[1]);
 	return 0;
 }
